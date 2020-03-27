62f0ed18951001d6d2102b96e7345132e8998422
Mikko <mikko@pop-os.localdomain>
2020-03-27 12:34:18 +0200

    small fixes

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
           url=URL,
           version=VERSION,
           download_url=DOWNLOAD_URL,
-          install_requires=['pmlb', 'pandas', 'twintel'],
+          install_requires=['pmlb', 'pandas', 'twintel', 'pymed'],
           packages=['dedomena',
                     'dedomena.apis',
                     'dedomena.datasets',
